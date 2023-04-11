de6a9467d7cb32cd4a509fb160548d30c066a9d6
Vesstnik87 <128038285+Vesstnik87@users.noreply.github.com>
2023-04-11 16:24:49 +0500

    321

--- buavr2022/settings.py
+++ buavr2022/settings.py
@@ -84,7 +84,7 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_test.sqlite3',
     }
 }
 
